86330752808d87b2689059bf1794c6c0c4d4cc52
Andy Wingo <wingo@igalia.com>
2022-10-13 13:46:30 +0200

    [WebAssembly] Add string.measure_* intrinsics and instructions
    From the [stringref](https://github.com/WebAssembly/stringref) proposal.

--- llvm/test/MC/WebAssembly/stringref.s
+++ llvm/test/MC/WebAssembly/stringref.s
@@ -48,3 +48,33 @@ string_new_wtf16:
 # CHECK-NEXT:        20 01      local.get 1
 # CHECK-NEXT:        fb 81 01 00  	string.new_wtf16
 # CHECK-NEXT:        0b           	end
+
+string_measure_utf8:
+  .functype string_measure_utf8 (stringref) -> (i32)
+  local.get 0
+  string.measure_utf8
+  end_function
+# CHECK: <string_measure_utf8>:
+# CHECK:             20 00	local.get	0
+# CHECK-NEXT:        fb 83 01  	string.measure_utf8
+# CHECK-NEXT:        0b           	end
+
+string_measure_wtf8:
+  .functype string_measure_wtf8 (stringref) -> (i32)
+  local.get 0
+  string.measure_wtf8
+  end_function
+# CHECK: <string_measure_wtf8>:
+# CHECK:             20 00	local.get	0
+# CHECK-NEXT:        fb 84 01  	string.measure_wtf8
+# CHECK-NEXT:        0b           	end
+
+string_measure_wtf16:
+  .functype string_measure_wtf16 (stringref) -> (i32)
+  local.get 0
+  string.measure_wtf16
+  end_function
+# CHECK: <string_measure_wtf16>:
+# CHECK:             20 00	local.get	0
+# CHECK-NEXT:        fb 85 01  	string.measure_wtf16
+# CHECK-NEXT:        0b           	end
